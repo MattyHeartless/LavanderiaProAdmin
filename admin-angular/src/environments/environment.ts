a89e2baf1b711f280a0e47a76aa07b58519f6b4a
Jair Mancilla Molina <jaiman@MacBook-Pro-de-Jair.local>
2026-09-08 21:25:08 -0600

    minor changes

--- admin-angular/src/environments/environment.ts
+++ admin-angular/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  authApiUrl: 'http://localhost:5116/api/Auth',
-  profileApiUrl: '',
-  catalogsApiUrl: 'http://localhost:5009/api/Catalogs',
+  authApiUrl: 'http://localhost:5003/api/Auth',
+  profileApiUrl: 'http://localhost:5002/api/Profile',
+  catalogsApiUrl: 'http://localhost:5001/api/Catalogs',
   ordersApiUrl: 'http://localhost:5252/api/Orders'
 };
