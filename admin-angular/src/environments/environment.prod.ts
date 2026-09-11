f379252a35a71e80312fff7a08950e251585565e
Jair Mancilla Molina <jaiman@MacBook-Pro-de-Jair.local>
2026-09-10 21:47:03 -0600

    production settings

--- admin-angular/src/environments/environment.prod.ts
+++ admin-angular/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  authApiUrl: 'https://ca-auth-api.ashywater-a0d6b972.westus2.azurecontainerapps.io/api/Auth',
-  profileApiUrl: 'https://profile-service.ashywater-a0d6b972.westus2.azurecontainerapps.io/api/Profile',
-  catalogsApiUrl: 'https://catalogs-service.ashywater-a0d6b972.westus2.azurecontainerapps.io/api/Catalogs',
-  ordersApiUrl: 'https://orders.ashywater-a0d6b972.westus2.azurecontainerapps.io/api/Orders'
+  authApiUrl: 'http://auth-mhm0d92ttfsc887lizhbqobe.5.78.222.52.sslip.io/api/Auth',
+  profileApiUrl: 'http://profile-odc45xuaftxe4vc8ln3ut4xi.5.78.222.52.sslip.io/api/Profile',
+  catalogsApiUrl: 'http://catalogs-rzm1yxr4ri92lwl3ebswrd8u.5.78.222.52.sslip.io/api/Catalogs',
+  ordersApiUrl: 'http://orders-qcy3lpqvgvjumcg245cppo4i.5.78.222.52.sslip.io/api/Orders'
 };
